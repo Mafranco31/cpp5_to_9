ec3927b84c4096533ab15a0aa6ae6e975ab2d34d
Mafranco31 <mat.francois31@gmail.com>
2024-07-24 14:58:30 +0200

    cpp_7_correction

--- cpp07/ex00/utils.hpp
+++ cpp07/ex00/utils.hpp
@@ -3,10 +3,22 @@
 
 # include <iostream>
 # include <string>
-# include "utils.cpp"
 
-template <typename T> void swap(T &lhs, T &rhs);
-template <typename T> T min(const T &lhs, const T &rhs);
-template <typename T> T max(const T &lhs, const T &rhs);
+template <typename T> void swap(T &lhs, T &rhs) {
+    T temp;
+
+    temp = lhs;
+    lhs = rhs;
+    
+    rhs = temp;
+}
+
+template <typename T> T min(const T &lhs, const T &rhs) {
+    return lhs < rhs ? lhs : rhs;
+}
+
+template <typename T> T max(const T &lhs, const T &rhs) {
+    return lhs > rhs ? lhs : rhs;
+}
 
 #endif
