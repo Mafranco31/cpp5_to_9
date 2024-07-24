121a0ef0ff365a3cd2de51cf91c733ebec1373f5
Mafranco31 <mat.francois31@gmail.com>
2024-07-24 14:54:46 +0200

    cpp7_ex00

---
+++ cpp07/ex00/utils.hpp
@@ -0,0 +1,12 @@
+#ifndef UTILS_HPP
+# define UTILS_HPP
+
+# include <iostream>
+# include <string>
+# include "utils.cpp"
+
+template <typename T> void swap(T &lhs, T &rhs);
+template <typename T> T min(const T &lhs, const T &rhs);
+template <typename T> T max(const T &lhs, const T &rhs);
+
+#endif
